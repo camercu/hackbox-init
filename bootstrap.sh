2ab72b46a63352a1bea02845e236f3dbf02bc4fc
camercu <cameron.unterberger@gmail.com>
2022-07-13 16:20:11 -0400

    change hypervisor detection

--- bootstrap.sh
+++ bootstrap.sh
@@ -34,24 +34,32 @@ if [[ "$UID" != 0 ]]; then
     exit $?
 fi
 
-in_vmware=$(lspci | grep -i vmware &>/dev/null && echo true || echo false )
-if [[ $in_vmware == true ]]; then
-    info "Ensuring VMWare Tools are installed..."
-    apt update && apt install -y open-vm-tools fuse3
+info "Updating apt cache..."
+apt update
 
-    info "Ensuring shared folder is mounted..."
-    host_dir="$(awk '{print $1}' $HERE/fstab)"
-    if ! grep -F "$host_dir" /etc/fstab &>/dev/null; then
-        cat /etc/fstab "$HERE/fstab" > /tmp/fstab.new
-        mv /tmp/fstab.new /etc/fstab
-        mountpt="$(awk '{print $2}' "$HERE/fstab")"
-        mkdir -p "$mountpt"
-        mount -a
+in_vm=$(grep flags /proc/cpuinfo 2>/dev/null | grep hypervisor &>/dev/null && echo true || echo false )
+hypervisor="$(cat /sys/devices/virtual/dmi/id/product_name)"
+if [[ $in_vm == true ]]; then
+    if [[ "$hypervisor" == VMWare* ]]; then
+        info "Ensuring VMWare Tools are installed..."
+        apt install -y open-vm-tools fuse3
+
+        info "Ensuring shared folder is mounted..."
+        host_dir="$(awk '{print $1}' $HERE/fstab)"
+        if ! grep -F "$host_dir" /etc/fstab &>/dev/null; then
+            cat /etc/fstab "$HERE/fstab" > /tmp/fstab.new
+            mv /tmp/fstab.new /etc/fstab
+            mountpt="$(awk '{print $2}' "$HERE/fstab")"
+            mkdir -p "$mountpt"
+            mount -a
+        fi
+    elif [[ "$hypervisor" == "VirtualBox" ]]; then
+        apt install -y build-essential dkms #linux-headers-$(uname -r)
     fi
 fi
 
 info "Installing prerequisites for Ansible..."
-apt update && apt install -y python3 python3-pip
+apt install -y python3 python3-pip
 
 info "Installing Ansible..."
 python3 -m pip install -U ansible
@@ -59,13 +67,13 @@ python3 -m pip install -U ansible
 info "Running Ansible script..."
 ansible-playbook -v -i localhost, --connection=local -e "ansible_python_interpreter=$(which python3)" "$HERE/ansible/hackbox-init.yml"
 
-if [[ $in_vmware == true && -d /mnt/share/.dotfiles ]]; then
+if [[ "$hypervisor" == VMWare* && -d /mnt/share/.dotfiles ]]; then
     info "Swapping out dotfile dir for shared copy..."
     [[ -d /home/kali/.dotfiles && ! -L /home/kali/.dotfiles ]] && rm -rf /home/kali/.dotfiles
     ln -sf /mnt/share/.dotfiles /home/kali/.dotfiles
 fi
 
-if [[ $in_vmware == true && -d /mnt/share/hackbox-init ]]; then
+if [[ "$hypervisor" == VMWare* && -d /mnt/share/hackbox-init ]]; then
     info "Swapping out hackbox-init dir for shared copy..."
     [[ -d /home/kali/hackbox-init && ! -L /home/kali/hackbox-init ]] && rm -rf /home/kali/hackbox-init
     ln -sf /mnt/share/hackbox-init /home/kali/hackbox-init
